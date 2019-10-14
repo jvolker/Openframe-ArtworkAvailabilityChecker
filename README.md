c0dff1f422ed8f0ab546afe559e285b93ada86d4
Jeremias Volker <jeremias.volker@googlemail.com>
2019-10-15 00:20:19 +0200

    Add statistics about thumbnails and duplicate artwork urls

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Checks availability of public artworks in an [Openframe](http://openframe.io) database.
 
-It requests all public artwork from the server using [Openframe-JSClient](https://github.com/OpenframeProject/Openframe-JSClient). It then checks every artwork URL. In case it doesn't return with HTTP-Statuscode 200 (OK) an error is displayed. It also counts how many are available / failed.
+It requests all public artwork from the server using [Openframe-JSClient](https://github.com/OpenframeProject/Openframe-JSClient). It then checks every artwork URL. In case it doesn't return with HTTP-Statuscode 200 (OK) an error is displayed. It also shows some statistics.
 
 This should be extended in the future to implement https://github.com/OpenframeProject/Openframe-WebApp/issues/7 and push back the results to the API.
 @jmwohl:
@@ -14,3 +14,53 @@ This should be extended in the future to implement https://github.com/OpenframeP
 # Run
 
 `node checkAvailability`
+
+# Example output
+
+Output on 15th Oct 2019:
+```
+Found 324 public artworks.
+
+57841827c0006da8310e8e69 Invalid URL meetar.github.io/terrain-tour
+56e4dc3f44973147579abbef ENOTFOUND server can not be found 
+57041cdee1f87ce61cc0af07 ENOTFOUND server can not be found 
+572114b0c2cb33000be1eea2 ENOTFOUND server can not be found 
+570c0c7c708dc5311ca1e4c8 404 Not found 
+56e0c15e17bbab454407c2b7 404 Not found 
+57640f99c0006da8310e8a90 404 Not found 
+576ae072c0006da8310e8b89 404 Not found 
+570f716e507bfb8922c89814 403 Forbidden 
+57640ff1c0006da8310e8a94 404 Not found 
+57a85d5bc0006da8310e906b 404 Not found 
+59711251b2462d7382b8f530 404 Not found 
+5970fe40b2462d7382b8f52f 404 Not found 
+5972196db2462d7382b8f53e 404 Not found 
+59711293b2462d7382b8f534 404 Not found 
+588d38697cb7f28d67893075 404 Not found 
+576994d0c0006da8310e8b6c 404 Not found 
+597219fbb2462d7382b8f53f 404 Not found 
+5a7876c3b2462d7382b8fa63 403 Forbidden 
+56e5c923a6b560d606184662 404 Not found 
+5cd6da5aa38167076035bc00 403 Forbidden 
+5af67553a38167076035b505 403 Forbidden 
+5af67576a38167076035b506 403 Forbidden 
+5af67510a38167076035b504 403 Forbidden 
+5896d4c6a9c1b11803b240fb 404 Not found 
+59b615d1b2462d7382b8f5df 404 Not found 
+59b615adb2462d7382b8f5de 404 Not found 
+57846752c0006da8310e8e89 404 Not found 
+584d65957cb7f28d67892d8e 403 Forbidden 
+59b61573b2462d7382b8f5dd 404 Not found 
+5b3d1c31a38167076035b5d6 403 Forbidden 
+
+293/324 artworks available
+31/324 artworks unavailable
+283/324 thumbnails available
+
+Duplicate artwork urls:
+https://thebookofshaders.com/log/160306213426.frag: 2
+https://goo.gl/images/Y1weHm: 2
+https://goo.gl/images/j9YfL2: 2
+https://thebookofshaders.com/log/160414134236.frag: 2
+
+```
--- checkAvailability.js
+++ checkAvailability.js
@@ -12,16 +12,98 @@ var request = require('request');
 
 console.log(chalk.yellow('=====================================================================')); // just a visual divider on the console 
 
-OF.users.login(credentials)
-  .then(token => {
-    // console.log(token);
-  });
-
 let artworks
-let available = 0
-let artworkTypes = []
+let artworkAvailable = 0
+let thumbnailsAvailable = 0
+let artworkTypeCounts = []
+let artworkUrlCounts = []
 
-OF.artwork.fetch({
+function checkArtwork(artwork) {
+  let artworkRequest = request({
+      url: artwork.url,
+      // maxAttempts: 3,   // (default) try 3 times
+      // retryDelay: 2000  // (default) wait for 3s before trying again
+    })
+    .on('error', function(err) {
+      if (err.errno = 'ENOTFOUND') {
+        console.log(artwork.id + chalk.red(' ENOTFOUND server can not be found '))
+        
+      } else {
+        console.log(artwork.id + chalk.red(' error ')  + artwork.url)
+        console.log(err)            
+      }
+    })
+    .on('response', function(_response) {
+      response = _response
+      
+      if (response.statusCode == 200) {
+        // console.log(artwork.id + chalk.green(' available'))
+        artworkAvailable++            
+        
+        if (!artworkTypeCounts[artwork.format]) artworkTypeCounts[artwork.format] = 0;
+        if (!artworkUrlCounts[artwork.url]) artworkUrlCounts[artwork.url] = 0;
+        
+        artworkTypeCounts[artwork.format]++
+        artworkUrlCounts[artwork.url]++
+      }
+      else if (response.statusCode == 403) {
+        console.log(artwork.id + chalk.red(' ' + response.statusCode + ' Forbidden '))
+        // this.about
+      }
+      else if (response.statusCode == 404) {
+        console.log(artwork.id + chalk.red(' ' + response.statusCode + ' Not found '))
+        // this.about
+      }
+      else {
+        console.log(artwork.id + chalk.yellow(' ' + response.statusCode + ' unsure ')  + ' ' + artwork.url)
+      } 
+      
+      artworkRequest.abort()
+      // else if (response.statusCode == 400) {
+      //   console.log(artwork.id + " failed. Will try again");
+      //   // this.about
+      // }
+      
+    })
+    .on('end', function(){
+      
+    })
+}
+
+function checkThumbnail(artwork) {
+  if (artwork.thumb_url) {
+    let thumbnailRequest = request({
+        url: artwork.thumb_url,
+        // maxAttempts: 3,   // (default) try 3 times
+        // retryDelay: 2000  // (default) wait for 3s before trying again
+      })
+      .on('error', function(err) {
+        if (err.errno = 'ENOTFOUND') {
+          // console.log(artwork.id + chalk.red(' ENOTFOUND server can not be found '))
+          
+        } else {
+          // console.log(artwork.id + chalk.red(' error ')  + artwork.url)
+          // console.log(err)            
+        }
+      })
+      .on('response', function(_response) {
+        response = _response
+        
+        if (response.statusCode == 200) {
+          // console.log(artwork.id + chalk.green(' available'))
+          thumbnailsAvailable++        
+        }
+        thumbnailRequest.abort()
+      })
+  } 
+}
+
+
+// Login is not needed to read public artworks but in order to return the results later on
+// OF.users.login(credentials)
+//   .then(token => {
+//     // console.log(token);
+  OF.artwork.fetch({
     limit: 0,                                
     where: {                                 
       is_public : true                          
@@ -39,56 +121,15 @@ OF.artwork.fetch({
         console.log(artwork.id + chalk.red(' Invalid URL ') + artwork.url)
         continue
       }
-      
-      let artworkRequest = request({
-          url: artwork.url,
-          // maxAttempts: 3,   // (default) try 3 times
-          // retryDelay: 2000  // (default) wait for 3s before trying again
-        })
-        .on('error', function(err) {
-          if (err.errno = 'ENOTFOUND') {
-            console.log(artwork.id + chalk.red(' ENOTFOUND server can not be found '))
-            
-          } else {
-            console.log(artwork.id + chalk.red(' error ')  + artwork.url)
-            console.log(err)            
-          }
-        })
-        .on('response', function(_response) {
-          response = _response
-          
-          if (response.statusCode == 200) {
-            // console.log(artwork.id + chalk.green(' available'))
-            available++            
-            
-            if (!artworkTypes[artwork.format]) artworkTypes[artwork.format] = 0;
-            
-            artworkTypes[artwork.format]++
-          }
-          else if (response.statusCode == 403) {
-            console.log(artwork.id + chalk.red(' ' + response.statusCode + ' Forbidden '))
-            // this.about
-          }
-          else if (response.statusCode == 404) {
-            console.log(artwork.id + chalk.red(' ' + response.statusCode + ' Not found '))
-            // this.about
-          }
-          else {
-            console.log(artwork.id + chalk.yellow(' ' + response.statusCode + ' unsure ')  + ' ' + artwork.url)
-          } 
-          
-          artworkRequest.abort()
-          // else if (response.statusCode == 400) {
-          //   console.log(artwork.id + " failed. Will try again");
-          //   // this.about
-          // }
-          
-        })
-        .on('end', function(){
-          
-        })
+    
+      checkArtwork(artwork)
+      checkThumbnail(artwork)
+
     }
   });
+  // });
+
+
 
 // TODO: this currently throws an error
 // OF.users.logout()
@@ -106,12 +147,24 @@ process.on ('exit', code => {
     console.log ('Hmm something went wrong');
   } else {
     console.log()
-    console.log(chalk.green(available) + '/' + chalk.magenta(artworks.length) + ' artworks available')
-    console.log(chalk.red(artworks.length - available) + ' artworks unavailable')
-    console.log()    
-    let artworkTypesKeysOrdered = Object.keys(artworkTypes).sort(function(a, b) {return -(artworkTypes[a] - artworkTypes[b])})
-    for (let index of artworkTypesKeysOrdered) {
-      console.log(index + ': ' + chalk.yellow(artworkTypes[index])) 
+    console.log(chalk.green(artworkAvailable) + '/' + artworks.length + ' artworks available')
+    console.log(chalk.red(artworks.length - artworkAvailable) + '/' + artworks.length + ' artworks unavailable')
+    console.log(chalk.green(thumbnailsAvailable) + '/' + artworks.length + ' thumbnails available')
+    
+    // artwork counts
+    console.log()
+    let artworkTypeCountsSorted = artworkTypeCounts.sort((a,b) => -(artworkTypeCounts[a] - artworkTypeCounts[b]))
+    for (let index of artworkTypeCounts) {
+      let count = artworkTypeCounts[index]
+      console.log(index + ': ' + chalk.yellow(count)) 
+    }
+    
+    // duplicate artwork urls
+    console.log('Duplicate artwork URLs:')
+    let artworkUrlCountsSorted = artworkUrlCounts.sort((a,b) => artworkUrlCounts[a] - artworkUrlCounts[b])
+    for (let artworkUrl in artworkUrlCountsSorted) {
+      let count = artworkUrlCountsSorted[artworkUrl]
+      if (count > 1) console.log(artworkUrl + ': ' + chalk.yellow(count))
     }
   }
 });
